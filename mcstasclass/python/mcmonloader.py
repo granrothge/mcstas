d3e30023ba9b26b84916d15ea027e8a7d89d7eaa
Granroth, Garrett E <granrothge@ornl.gov>
2019-09-22 16:08:28 -0400

    bin runs

--- mcstasclass/python/mcmonloader.py
+++ mcstasclass/python/mcmonloader.py
@@ -101,6 +101,7 @@ class Data1D(DataMcCode):
         ax.set_xlabel(self.xlabel)
         ax.set_ylabel(self.ylabel)
         ax.set_title(self.title)
+
     def bin(self, binwidth):
         """
         rebins the data into bins of width binwidth
@@ -108,9 +109,9 @@ class Data1D(DataMcCode):
         # Do we need to do type checking
         outcls = self.clone()
         xidx_sorted = np.argsort(self.xvals)
-        x = self.xvals[xidx_sorted]
-        y = self.yvals[xidx_sorted]
-        e = self.y_err_vals[xidx_sorted]
+        x = np.array(self.xvals)[xidx_sorted]
+        y = np.array(self.yvals)[xidx_sorted]
+        e = np.array(self.y_err_vals)[xidx_sorted]
 
         # for storing the values to be combined
         xcombi = []
@@ -123,10 +124,10 @@ class Data1D(DataMcCode):
 
         for idx, xval in enumerate(x):
             if len(xcombi)>0:
-                if (x-xcombi[0])>binwidth:
+                if (xval-xcombi[0])>binwidth:
                     xres.append(np.sum(xcombi)/len(xcombi))
                     yres.append(np.sum(ycombi)/len(xcombi))
-                    eres.append(np.sqrt(np.sum(ecombi*ecombi))/len(xcombi))
+                    eres.append(np.sqrt(np.sum(np.array(ecombi)*np.array(ecombi)))/len(xcombi))
                     xcombi = [xval]
                     ycombi = [y[idx]]
                     ecombi = [e[idx]]
@@ -138,13 +139,13 @@ class Data1D(DataMcCode):
         if len(xcombi)>0:
             xres.append(np.sum(xcombi)/len(xcombi))
             yres.append(np.sum(ycombi)/len(xcombi))
-            eres.append(np.sqrt(np.sum(ecombi*ecombi))/len(xcombi))
+            eres.append(np.sqrt(np.sum(np.array(ecombi)*np.array(ecombi)))/len(xcombi))
         outcls.xvals = xres
         outcls.yvals = yres
         outcls.y_err_vals = eres
-        outcls.title = title+"bined to {}".format(binwidth)
+        outcls.title = outcls.title+"bined to {}".format(binwidth)
         outcls.xlimits = (np.min(xres),np.max(xres))
-return outcls
+        return outcls
 
 
 
