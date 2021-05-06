ac8dccad4c944b66162a24e140674e5aa06f18f1
Granroth, Garrett E <granrothge@ornl.gov>
2021-05-06 14:26:57 -0400

    added routine to process sim file

--- mcstasclass/python/mcmonloader.py
+++ mcstasclass/python/mcmonloader.py
@@ -521,3 +521,23 @@ def _get_filenames_from_mccodesim(mccodesim):
         else:
             filenames.append('No file')
     return filenames
+
+def parms_from_sim(flname,simbnds = {'begin sim': True, 'end sim': False}):
+    """
+    Get parameters from a sim file
+    flname: a path to a file name
+    simbnds: tags to decide where to look for parameters.
+    """
+    parms={}
+    with open(flname)as dsimh:
+        dotsims = dsimh.readlines()
+    sim_flag = False
+    for ln in dotsims:
+        for sim in simbnds.keys():
+            if sim in ln:
+                sim_flag = simbnds[sim]
+        if sim_flag:
+            if 'Param:' in ln:
+                parm = ln.split('Param:')[1].split('=')
+                parms[parm[0].strip()] = parm[1].strip()
+    return parms
